c16f52c1f08a6b39830f0d8a598933d5a2cc123d
allianad <32075424+allianad@users.noreply.github.com>
2017-10-10 14:00:05 -0600

    updated

---
+++ BMICalculator.Android/MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace BMICalculator.Android
+{
+    [Activity(Label = "BMICalculator.Android", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        Button calculateButton;
+        TextView resultTextView;
+        EditText heightEditText, weightEditText;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            Xamarin.Insights.Initialize(XamarinInsights.ApiKey, this);
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            calculateButton = FindViewById<Button>(BMICalculator.Android.Resources.Id.calculateButton);
+            resultTextView = FindViewById<TextView>(BMICalculator.Android.Resources.Id.resultTextView);
+            heightEditText = FindViewById<EditText>(BMICalculator.Android.Resources.Id.heightEditText);
+            weightEditText = FindViewById<EditText>(BMICalculator.Android.Resources.Id.weightEditText);
+
+            calculateButton.Click += CalculateButton_Click;
+        }
+
+        void CalculateButton_Click (object sender, System.EventArgs e)
+        {
+            float height = float.Parse (heightEditText.Text);
+            float weight = float.Parse (weightEditText.Text);
+
+            float bmi = (weight / (height * height));
+
+            resultTextView.Text = bmi.ToString ();
+
+        }
+    }
+}
