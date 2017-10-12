aeb159d258155854dc229aba07c5f7301b454bf9
allianad <32075424+allianad@users.noreply.github.com>
2017-10-12 13:41:59 -0600

    updated

--- BMIAndroid/Resources/Resource.designer.cs
+++ BMIAndroid/Resources/Resource.designer.cs
@@ -9,9 +9,9 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: global::Android.Runtime.ResourceDesignerAttribute("BMICalculator.Android.Resource", IsApplication=true)]
+[assembly: global::Android.Runtime.ResourceDesignerAttribute("BMIAndroid.Resource", IsApplication=true)]
 
-namespace BMICalculator.Android
+namespace BMIAndroid
 {
 	
 	
@@ -44,23 +44,8 @@ namespace BMICalculator.Android
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050004
-			public const int calculateButton = 2131034116;
-			
-			// aapt resource value: 0x7f050001
-			public const int heightEditText = 2131034113;
-			
-			// aapt resource value: 0x7f050005
-			public const int resultTextView = 2131034117;
-			
 			// aapt resource value: 0x7f050000
-			public const int textView1 = 2131034112;
-			
-			// aapt resource value: 0x7f050002
-			public const int textView2 = 2131034114;
-			
-			// aapt resource value: 0x7f050003
-			public const int weightEditText = 2131034115;
+			public const int myButton = 2131034112;
 			
 			static Id()
 			{
